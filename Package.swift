c58afb4ec7369a14dcb08d96d506b17c5f7a235c
Vadym Markov <markov.vadym@gmail.com>
2022-04-01 20:20:12 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "git@github.com:pexip/webrtc-ios-builds/releases/download/webrtc-2021.08.18-cat/WebRTC.xcframework.zip",
+            url: "https://github.com/pexip/webrtc-ios-builds/releases/download/webrtc-2021.08.18-cat/WebRTC.xcframework.zip",
             checksum: "6965b0b8660080797647372c5806b425f8dbb97bb5d8fe1cf4cee753a7dc512e"
         ),
     ]
