74e4f35f64003365f85ff0a7989a478c0e9fc1ec
Vadym Markov <markov.vadym@gmail.com>
2022-04-04 01:26:17 +0200

    "Update Package.swift for M100"

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://api.github.com/repos/pexip/webrtc-ios-builds/releases/assets/43681815.zip",
-            checksum: "6965b0b8660080797647372c5806b425f8dbb97bb5d8fe1cf4cee753a7dc512e"
+            url: "https://api.github.com/repos/pexip/webrtc-ios-builds/releases/assets/61456711",
+            checksum: "48bfa3b63dc5cb829570108e59cbd069d7d8446f4da0e1ea46a8e32cd547d71b"
         ),
     ]
 )
