b8fb322c686ee330414265cbcf66ffaa6732e351
Vadym Markov <vadym.markov@pexip.com>
2023-02-09 16:16:50 +0100

    Fix binary target url for M108

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://api.github.com/repos/pexip/webrtc-ios-builds/releases/assets/86675907.zip",
+            url: "https://github.com/pexip/webrtc-objc/releases/download/108.0.5359/WebRTC-universal_dsyms.zip",
             checksum: "9178e1a2623c7215c9313fa4e6710a9a209badc8d99cee85f3e732b68c1d9675"
         ),
     ]
