c412cdb011d76fa2235be84d128652034cf4d1b8
Vadym Markov <vadym.markov@pexip.com>
2023-08-08 16:54:22 +0200

    "Update Package.swift for M115"

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://api.github.com/repos/pexip/webrtc-objc/releases/assets/120635321.zip",
+            url: "https://api.github.com/repos/pexip/webrtc-objc/releases/assets/120635697.zip",
             checksum: "2deb321d5d59248a753ad77d8d96e24acf71bff08616cc0622107a1d20bdccd0"
         ),
     ]
